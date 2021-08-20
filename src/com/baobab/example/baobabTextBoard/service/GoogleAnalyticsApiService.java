667ac895df7ff1383024c11e7f3edd6c23d4cebf
HyeSeong123 <banggu1997@gmail.com>
2021-08-20 16:33:50 +0900

    게시물 추가

--- src/com/baobab/example/baobabTextBoard/service/GoogleAnalyticsApiService.java
+++ src/com/baobab/example/baobabTextBoard/service/GoogleAnalyticsApiService.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 import com.baobab.example.baobabTextBoard.Container;
 import com.baobab.example.baobabTextBoard.dao.Ga4DataDao;
-import com.google.analytics.data.v1alpha.AlphaAnalyticsDataClient;
-import com.google.analytics.data.v1alpha.DateRange;
-import com.google.analytics.data.v1alpha.Dimension;
-import com.google.analytics.data.v1alpha.Entity;
-import com.google.analytics.data.v1alpha.Metric;
-import com.google.analytics.data.v1alpha.Row;
-import com.google.analytics.data.v1alpha.RunReportRequest;
-import com.google.analytics.data.v1alpha.RunReportResponse;
+import com.google.analytics.data.v1beta.BetaAnalyticsDataClient;
+import com.google.analytics.data.v1beta.DateRange;
+import com.google.analytics.data.v1beta.Dimension;
+import com.google.analytics.data.v1beta.Metric;
+import com.google.analytics.data.v1beta.Row;
+import com.google.analytics.data.v1beta.RunReportRequest;
+import com.google.analytics.data.v1beta.RunReportResponse;
+
 
 public class GoogleAnalyticsApiService {
 	private Ga4DataDao ga4DataDao;
@@ -21,12 +21,12 @@ public class GoogleAnalyticsApiService {
 	}
 	public boolean updateGa4DataPageHits() {
 		String ga4PropertyId = Container.config.getGa4PropertyId();
-		try (AlphaAnalyticsDataClient analyticsData = AlphaAnalyticsDataClient.create()) {
+		try (BetaAnalyticsDataClient  analyticsData = BetaAnalyticsDataClient.create()) {
 			RunReportRequest request = RunReportRequest.newBuilder()
-					.setEntity(Entity.newBuilder().setPropertyId(ga4PropertyId))
+					.setProperty("properties/" + ga4PropertyId)
 					.addDimensions(Dimension.newBuilder().setName("pagePath"))
 					.addMetrics(Metric.newBuilder().setName("activeUsers"))
-					.addDateRanges(DateRange.newBuilder().setStartDate("2021-06-22").setEndDate("today")).setLimit(-1).build();
+					.addDateRanges(DateRange.newBuilder().setStartDate("2021-06-22").setEndDate("today")).build();
 
 			// Make the request
 			RunReportResponse response = analyticsData.runReport(request);
@@ -46,6 +46,7 @@ public class GoogleAnalyticsApiService {
 		System.out.println("성공");
 		return true;
 	}
+	
 	private void update(String pagePath, int hit) {
 		 ga4DataDao.deletePagePath(pagePath);
 		 ga4DataDao.savaPagePath(pagePath, hit);
