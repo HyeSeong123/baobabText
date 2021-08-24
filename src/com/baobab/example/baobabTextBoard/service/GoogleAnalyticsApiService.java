5ed37dacdb16e0d115150b369586b1a664d2f3e4
HyeSeong123 <banggu1997@gmail.com>
2021-08-25 00:06:46 +0900

    애널리틱스 설정 변경

--- src/com/baobab/example/baobabTextBoard/service/GoogleAnalyticsApiService.java
+++ src/com/baobab/example/baobabTextBoard/service/GoogleAnalyticsApiService.java
@@ -12,21 +12,20 @@ import com.google.analytics.data.v1beta.Row;
 import com.google.analytics.data.v1beta.RunReportRequest;
 import com.google.analytics.data.v1beta.RunReportResponse;
 
-
 public class GoogleAnalyticsApiService {
 	private Ga4DataDao ga4DataDao;
-	
-	public GoogleAnalyticsApiService(){
+
+	public GoogleAnalyticsApiService() {
 		ga4DataDao = new Ga4DataDao();
 	}
+
 	public boolean updateGa4DataPageHits() {
 		String ga4PropertyId = Container.config.getGa4PropertyId();
-		try (BetaAnalyticsDataClient  analyticsData = BetaAnalyticsDataClient.create()) {
-			RunReportRequest request = RunReportRequest.newBuilder()
-					.setProperty("properties/" + ga4PropertyId)
+		try (BetaAnalyticsDataClient analyticsData = BetaAnalyticsDataClient.create()) {
+			RunReportRequest request = RunReportRequest.newBuilder().setProperty("properties/" + ga4PropertyId)
 					.addDimensions(Dimension.newBuilder().setName("pagePath"))
-					.addMetrics(Metric.newBuilder().setName("activeUsers"))
-					.addDateRanges(DateRange.newBuilder().setStartDate("2021-06-22").setEndDate("today")).build();
+					.addMetrics(Metric.newBuilder().setName("screenPageViews"))
+					.addDateRanges(DateRange.newBuilder().setStartDate("2021-07-21").setEndDate("today")).build();
 
 			// Make the request
 			RunReportResponse response = analyticsData.runReport(request);
@@ -36,21 +35,23 @@ public class GoogleAnalyticsApiService {
 				String pagePath = row.getDimensionValues(0).getValue();
 				int hit = Integer.parseInt(row.getMetricValues(0).getValue());
 				System.out.printf("pagePath : %s, hit : %d\n", pagePath, hit);
-				
+
 				update(pagePath, hit);
 			}
 		} catch (IOException e) {
+			System.out.println("에러= " + e);
 			System.out.println("실패");
 			return false;
 		}
 		System.out.println("성공");
 		return true;
 	}
-	
+
 	private void update(String pagePath, int hit) {
-		 ga4DataDao.deletePagePath(pagePath);
-		 ga4DataDao.savaPagePath(pagePath, hit);
+		ga4DataDao.deletePagePath(pagePath);
+		ga4DataDao.savaPagePath(pagePath, hit);
 	}
+
 	public void updatePageHits() {
 		updateGa4DataPageHits();
 		Container.articleService.updatePageHits();
